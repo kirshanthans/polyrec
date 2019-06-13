392082f9413c09590c6141fbdcab26696bb51d35
kirshanthans <ksundar@purdue.edu>
2019-06-13 10:35:20 -0400

    demo.py added

--- demo.py
+++ demo.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python
+from transformations import Transformation
+from witnesstuples import WitnessTuple
+from dependencetest import Dependence
+from completion import Completion
+from astxforms import ASTXform
+from ast import *
+
+def demo():
+    p = nest()
+    # info about the program
+    dim      = p.getdim()
+    dim_type = p.getdimtype()
+    in_alp   = p.getalp()
+    in_ord   = p.getord()
+
+if __name__ == "__main__":
+    demo()
+
