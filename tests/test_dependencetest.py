16a89683ef4e3044dcb5b936738a73ac9550a430
kirshanthans <ksundar@purdue.edu>
2019-07-23 17:25:32 -0400

    test environment

---
+++ tests/test_dependencetest.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+import pytest
+from polyrec.dependencetest import Path, Dependence
---
+++ tests/test_statemachines.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+import pytest
+from polyrec.statemachines import MultiTapeFSA, MultiTapeFST
