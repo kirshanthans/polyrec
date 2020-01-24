04fa255c2986d79718af5b9d6d46db4a9d4d3558
kirshanthans <ksundar@purdue.edu>
2020-01-23 22:43:19 -0500

    restructure

--- examples/parser.py
+++
@@ -1,9 +0,0 @@
-import sys
-from pycparser import c_parser, c_ast
-
-with open('examples/c_files/test.c', 'r') as f:
-    text = f.read()
-
-parser = c_parser.CParser()
-ast = parser.parse(text)
-ast.show()
