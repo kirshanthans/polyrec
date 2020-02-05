90a5c0bc7abf2cd232eed92734c3e0b00136fd09
kirshanthans <ksundar@purdue.edu>
2020-02-04 19:28:09 -0500

    refactoring

--- polyrec/astxforms.py
+++ polyrec/astxforms.py
@@ -1,6 +1,6 @@
 import sys, os
 from polyrec.transformations import Transformation
-from polyrec.ast import *
+from polyrec.ast import CallExpr, IfStmt, ReturnStmt, Assignment, BinOp, Array, Field, Number, Const, Var
 
 def switch_tags(nodes, t1, t2):
     if isinstance(nodes, list):
